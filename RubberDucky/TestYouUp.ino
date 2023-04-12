b10ae802c7aaac35ffd72e9e046bc8437f2085e3
th3spis <69475918+th3spis@users.noreply.github.com>
2023-04-12 19:49:33 +0200

    Create TestYouUp.ino

---
+++ RubberDucky/TestYouUp.ino
@@ -0,0 +1,46 @@
+/**
+ * Made with Duckuino, an open-source project.
+ * Check the license at 'https://github.com/Dukweeno/Duckuino/blob/master/LICENSE'
+ */
+
+#include "Keyboard.h"
+
+void typeKey(uint8_t key)
+{
+  Keyboard.press(key);
+  delay(50);
+  Keyboard.release(key);
+}
+
+/* Init function */
+void setup()
+{
+  // Begining the Keyboard stream
+  Keyboard.begin();
+
+  // Wait 500ms
+  delay(500);
+
+  delay(3000);
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+
+  delay(200);
+  //Keyboard.print(F("-/"));
+  // Changed the variables from files in C:\Users\XXXX\AppData\Local\Arduino15\libraries\Keyboard\src
+  // Keyboard.h ---> *layout = KeyboardLayout_es_ES
+  // Keyboard.cpp --> _asciimap = KeyboardLayout_es_ES;
+  Keyboard.print(F("https://www.youtube.com/watch?v=dQw4w9WgXcQ"));
+
+  typeKey(KEY_RETURN);
+
+  delay(3000);
+  Keyboard.print(F("f")); //Autoplay
+
+  // Ending stream
+  Keyboard.end();
+}
+
+/* Unused endless loop */
+void loop() {}
